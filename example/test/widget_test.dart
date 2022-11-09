839848c2dcb77c8f992f1c5fd636accb08b427e7
Rizvan Adzhoyan <radzhoyan@gmail.com>
2022-11-09 10:03:03 +0600

    up blue_thermal_printer version remove unused views

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -5,14 +5,12 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:blue_print_pos_example/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets('Verify Platform version', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
 
     // Verify that platform version is retrieved.
     expect(
