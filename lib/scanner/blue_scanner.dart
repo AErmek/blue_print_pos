09ef5216ed8f8518fe898f23f36e99348777d7d4
akmatove <ermek_akmatov@inbox.ru>
2022-12-07 12:32:43 +0600

    eliminate everything except flutter_blue_plus

--- lib/scanner/blue_scanner.dart
+++ lib/scanner/blue_scanner.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 import 'package:blue_print_pos/models/blue_device.dart';
-import 'package:blue_thermal_printer/blue_thermal_printer.dart' as blue_thermal;
 import 'package:flutter_blue_plus/flutter_blue_plus.dart' as flutter_blue;
 
 /// This class with static method to handler scanning in Android and iOS
@@ -12,16 +11,18 @@ class BlueScanner {
   static Future<List<BlueDevice>> scan() async {
     List<BlueDevice> devices = <BlueDevice>[];
     if (Platform.isAndroid) {
-      final blue_thermal.BlueThermalPrinter bluetoothAndroid =
-          blue_thermal.BlueThermalPrinter.instance;
-      final List<blue_thermal.BluetoothDevice> resultDevices =
-          await bluetoothAndroid.getBondedDevices();
+      final flutter_blue.FlutterBluePlus bluetoothIOS =
+          flutter_blue.FlutterBluePlus.instance;
+      final List<flutter_blue.BluetoothDevice> resultDevices =
+          await bluetoothIOS.bondedDevices;
       devices = resultDevices
+          .toSet()
+          .toList()
           .map(
-            (blue_thermal.BluetoothDevice bluetoothDevice) => BlueDevice(
-              name: bluetoothDevice.name ?? '',
-              address: bluetoothDevice.address ?? '',
-              type: bluetoothDevice.type,
+            (flutter_blue.BluetoothDevice bluetoothDevice) => BlueDevice(
+              address: bluetoothDevice.id.id,
+              name: bluetoothDevice.name,
+              type: bluetoothDevice.type.index,
             ),
           )
           .toList();
